1e4f61c3405c68f0da488d16957365fa580aab8e
Omar Alshaker <omar@omaralshaker.com>
2017-11-22 14:47:48 +0100

    Enhance popup UX

--- build/webextension/popup/popup.js
+++ build/webextension/popup/popup.js
@@ -1,9 +1,16 @@
 const handler = function(ev) {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { content: 'showDebugAid', type: ev.target.id }, function(
-      response
-    ) {});
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { content: 'showDebugAid', type: ev.target.id },
+      function(response) {}
+    );
+    window.close();
   });
 };
 document.querySelector('#overlay').addEventListener('click', handler);
 document.querySelector('#popup').addEventListener('click', handler);
+
+document.querySelector('.whole-insect').addEventListener('click', function() {
+  this.classList.add('walking');
+});
