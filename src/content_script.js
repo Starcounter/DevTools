65d801c7efc4c854f7d00ac0a57185ad6c33e378
Omar Alshaker <omar@omaralshaker.com>
2017-11-28 20:30:24 +0100

    Don't pollute non Starcounter pages

--- src/content_script.js
+++ src/content_script.js
@@ -1,56 +1,56 @@
 /* this file injects starcounter-debug-aid's logic inside the page, 
 most probably you'll neither need to understand nor to modify it. */
+if (document.querySelector('puppet-client, palindrom-client')) {
+  if (typeof chrome !== 'undefined' && typeof browser === 'undefined') {
+    var browser = chrome;
+  }
 
-if (typeof chrome !== 'undefined' && typeof browser === 'undefined') {
-  var browser = chrome;
-}
-
-if (
-  typeof browser !== 'undefined' &&
-  browser.runtime &&
-  browser.runtime.onMessage
-) {
-  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (!message || !message.content) return;
-    switch (message.content) {
-      case 'Hey! Are you a Starcounter app?':
-        sendResponse(
-          document.querySelector('puppet-client, palindrom-client')
-            ? 'Yup!'
-            : 'Nope!'
-        );
-        break;
-      case 'showDebugAid':
-        window.dispatchEvent(new CustomEvent('sc-debug-show-' + message.type));
-        break;
-    }
-  });
-
-  /* We need to import the built script instead of just running it. 
+  if (
+    typeof browser !== 'undefined' &&
+    browser.runtime &&
+    browser.runtime.onMessage
+  ) {
+    browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (!message || !message.content) return;
+      switch (message.content) {
+        case 'Hey! Are you a Starcounter app?':
+          sendResponse(
+            document.querySelector('puppet-client, palindrom-client')
+              ? 'Yup!'
+              : 'Nope!'
+          );
+          break;
+        case 'showDebugAid':
+          window.dispatchEvent(
+            new CustomEvent('sc-debug-show-' + message.type)
+          );
+          break;
+      }
+    });
+    /* We need to import the built script instead of just running it. 
      This gives up access to JS variables */
 
-  const popupIndexScriptUrl = browser.extension.getURL('ui-popup-build.js');
-  localStorage.setItem('scDebugPopupIndexScriptUrl', popupIndexScriptUrl);
+    const popupIndexScriptUrl = browser.extension.getURL('ui-popup-build.js');
+    localStorage.setItem('scDebugPopupIndexScriptUrl', popupIndexScriptUrl);
 
-  const url = browser.extension.getURL('injected_script.js');
-  const script = document.createElement('SCRIPT');
-  script.src = url;
-  script.onload = function() {
-    /* check if a popup is already open and reconnect to it */
-    if (
-      window.localStorage.getItem('starcounterDebug-popup-open') &&
-      window.localStorage.getItem('starcounterDebug-popup-open') !== 'false'
-    ) {
-      window.dispatchEvent(new CustomEvent('sc-debug-show-popup'));
-    }
-  };
-  document.body.appendChild(script);
+    const url = browser.extension.getURL('injected_script.js');
+    const script = document.createElement('SCRIPT');
+    script.src = url;
+    script.onload = function() {
+      /* check if a popup is already open and reconnect to it */
+      if (
+        window.localStorage.getItem('starcounterDebug-popup-open') &&
+        window.localStorage.getItem('starcounterDebug-popup-open') !== 'false'
+      ) {
+        window.dispatchEvent(new CustomEvent('sc-debug-show-popup'));
+      }
+    };
+    document.body.appendChild(script);
 
-  document.body.appendChild(
-    document.createElement(
-      'just-an-arbitrary-element-to-tell-sc-debug-aid-extension-was-installed'
-    )
-  );
-
- 
+    document.body.appendChild(
+      document.createElement(
+        'just-an-arbitrary-element-to-tell-sc-debug-aid-extension-was-installed'
+      )
+    );
+  }
 }
